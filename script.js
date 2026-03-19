cd3ec55bc0d577ebb3e1a6e96e4832fcbfcd5255
Radoslav Bartosevic <rad.bartosevic@gmail.com>
2026-03-19 17:53:44 +0200

    Fixed the beast bug (fr)

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ async function loadServants() {
         return {
             id: servant.id,
             name: servant.name,
-            class: (servant.className ? "beast" : "beast"),
+            class: (servant.className == "beast" ? "beast" : servant.className),
             rarity: servant.rarity,
             gender: (servant.gender || "unknown").toLowerCase().replace("gender", ""),
             img: getServantImage(servant),
